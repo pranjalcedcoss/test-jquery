f037df87345e995419264dc2dd32267431e5a581
pranjal <pranjalsingh2022sep@cedcoss.com>
2022-10-13 18:30:53 +0530

    dew

---
+++ src/script.js
@@ -0,0 +1,25 @@
+var one = $("#one");
+var two = $("#two");
+var three = $("#three");
+var four = $("#four");
+var five = $("#five");
+var six = $("#six");
+$(document).ready(function(){
+    $("#one").click(function(){
+     
+    $("#data").css("backgroundColor", "lightblue");},);
+    $("#two").click(function(){
+     
+        $("#data").css("backgroundColor", "blue");},);
+        $("#three").click(function(){
+     
+            $("#data").css("backgroundColor", "yellow");},);
+            $("#four").click(function(){
+     
+                $("#data").css("backgroundColor", "grey");},);
+                $("#five").click(function(){
+     
+                    $("#data").css("backgroundColor", "green");},);
+                    $("#six").click(function(){
+     
+                        $("#data").css("backgroundColor", "black");},);});
